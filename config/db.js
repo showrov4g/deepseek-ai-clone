9cd36f081ac72b157fb6c42ca84b21cb3d826e27
showrov4g <showrovg00@gmail.com>
2025-03-24 12:36:20 +0600

    mongodb database connection

---
+++ config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let cashed = global.mongoose || {conn: null, promise: null}
+
+
+export default async function connectDB(){
+    if(cashed.conn){
+        return cashed.conn
+    }
+    if(!cashed.promise){
+      cashed.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => mongoose)
+    }
+    try {
+        cashed.conn = await cashed.promise;
+    } catch (error) {
+        console.error("Error connection to mongodb", error)
+    }
+    return cashed.conn
+}
