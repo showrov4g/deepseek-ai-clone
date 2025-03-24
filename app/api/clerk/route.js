2e2d0734ddee26b79c63c5f0ba6bf6f2e569b586
showrov4g <showrovg00@gmail.com>
2025-03-24 19:42:36 +0600

    user data store model ready to use

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -1,18 +1,47 @@
 import { Webhook } from "svix";
 import connectDB from "@/config/db";
 import User from "@/models/User";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
+export async function POST(req) {
+  const wh = new Webhook(process.env.SIGNIN_SECRET);
+  const headerPayload = await headers();
+  const svixHeaders = {
+    svix_id: headerPayload.get("svix_id"),
+    svix_signature: headerPayload.get("svix_signature"),
+  };
+  // get preloader and verify it
 
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+  const { data, type } = wh.verify(body, svixHeaders);
 
-export async function POST(req){
-    const wh = new Webhook 
+  //prepare the user data to the data base
+
+  const userData = {
+    _id: data.id,
+    email: data.email_addresses[0].email_address,
+    name: `${data.first_name} ${data.last_name}`,
+    image: data.image_url,
+  };
+  await connectDB();
+
+  switch (type) {
+    case "user.created":
+      await User.create(userData);
+      break;
+
+    case "user.created":
+      await User.findByIdAndUpdate(data.id, userData);
+      break;
+
+      case "user.deleted":
+        await User.findByIdAndDelete(data.id);
+        break;
+
+    default:
+      break;
+  }
+  return NextRequest.json({message: "Event Received"})
 }
-
-
-
-
-
-
-
-
-
