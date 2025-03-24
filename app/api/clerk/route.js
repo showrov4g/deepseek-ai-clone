b0766aaad885b0002b58df2b410219a505f9e7ea
showrov4g <showrovg00@gmail.com>
2025-03-24 13:46:01 +0600

    api making for geting data from clerk and store data to the mongodb database

---
+++ app/api/clerk/route.js
@@ -0,0 +1,14 @@
+import { Webhook } from "svix";
+import connectDB from "@/config/db";
+import User from "@/models/User";
+
+
+
+
+
+
+
+
+
+
+
--- models/User.js
+++ models/User.js
@@ -12,5 +12,6 @@ const UserSchema = new mongoose.Schema(
   }
 );
 
+const User =mongoose.model.User || mongoose.model("User", UserSchema)
 
-
+export default User;
