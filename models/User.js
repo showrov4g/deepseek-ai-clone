faf58dac4770a22102c209bf808bc50f55328aef
showrov4g <showrovg00@gmail.com>
2025-03-24 22:00:31 +0600

    error fixed

--- models/User.js
+++ models/User.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    _id: { typeof: String, require: true },
-    name: { typeof: String, require: true },
-    email: { typeof: String, require: true },
-    image: { typeof: String, require: false },
+    _id: { typeof: String, require:true },
+    name: { typeof: String, require:true },
+    email: { typeof: String, require:true },
+    image: { typeof: String, require:false },
   },
   {
     timestamps: true,
