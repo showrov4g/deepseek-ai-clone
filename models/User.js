bfe60bd7ef8f9644b4d93d9635121425fbb8f69a
showrov4g <showrovg00@gmail.com>
2025-03-24 13:36:13 +0600

    user schema make successfull

--- models/User.js
+++ models/User.js
@@ -1,1 +1,16 @@
 import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    _id: { typeof: String, require: true },
+    name: { typeof: String, require: true },
+    email: { typeof: String, require: true },
+    image: { typeof: String, require: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+
