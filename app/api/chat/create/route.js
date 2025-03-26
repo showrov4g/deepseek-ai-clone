ee98afeccdd07655ea3f9967679730c1c62239f2
showrov4g <showrovg00@gmail.com>
2025-03-26 12:08:10 +0600

    caht data get api create'

--- app/api/chat/create/route.js
+++ app/api/chat/create/route.js
@@ -3,7 +3,7 @@ import Chat from "@/models/Chat";
 import { getAuth } from "@clerk/nextjs/dist/types/server";
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(req) {
   try {
     const { userId } = getAuth();
     if (!userId) {
--- app/api/chat/get/route.js
+++ app/api/chat/get/route.js
@@ -3,3 +3,19 @@ import Chat from "@/models/Chat";
 import { getAuth } from "@clerk/nextjs/dist/types/server";
 import { NextResponse } from "next/server";
 
+export async function GET(req) {
+  try {
+    const {userId} = getAuth();
+    if(!userId){
+        return NextResponse.json({success:false, message:"user is not Authorized"})
+    }
+    // connect database to get user chat data 
+    await connectDB();
+    const data = await Chat.find({userId});
+    return NextResponse.json({success:true, data})
+
+
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message });
+  }
+}
