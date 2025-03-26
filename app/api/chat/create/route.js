db4738a071457167d0354aa80f793d43ac3d4c63
showrov4g <showrovg00@gmail.com>
2025-03-26 11:34:07 +0600

    making chat create function

---
+++ app/api/chat/create/route.js
@@ -0,0 +1,11 @@
+import connectDB from "@/config/db";
+import Chat from "@/models/Chat";
+import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { NextResponse } from "next/server";
+
+export async function POST(){
+    
+}
+
+
+
